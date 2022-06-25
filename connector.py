99bf1fed3058d2940eba6f41bc1c69848aa82032
EvilzCoder <49076719+EvilzCoder@users.noreply.github.com>
2022-06-25 19:18:15 +0000

    Update connector.py

--- connector.py
+++ connector.py
@@ -3,6 +3,9 @@ from sys import argv
 
 query = str(input("mysql> "))
 host,port,user,passwd,db = argv[1],argv[2],argv[3],argv[4],argv[5]
+
+if len(sys.argv) < 5:
+    print(f"Usage: ./{sys.argv[0]} localhost 3306 'root' '' db_name")
 conn = pymysql.connect(host=host,port=int(port),user=user,passwd=passwd,db=db)
 _cursor = conn.cursor()
 
